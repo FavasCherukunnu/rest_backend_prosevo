f8bc93bad89b1857d3818358b93059e2955315e1
favas <mhdfavascheru@gmail.com>
2024-09-08 21:49:46 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const studentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  class: {
+    type: String,
+    required: true
+  },
+  subject: {
+    type: String,
+    required: true
+  }
+});
+
+const Student = mongoose.model('Student', studentSchema);
+
+app.use(bodyParser.json());
+app.get('/test', (req, res) => {
+  res.json({ message: 'Hello from the backend' });
+});
+app.get('/students', (req, res) => {
+  Student.find().then(students => res.json(students));
+});
+
+app.post('/students', (req, res) => {
+  Student.create(req.body).then(student => {
+    res.json(student);
+  }).catch(err => {
+    res.status(400).json({ message: 'Error creating student', errors: err.errors });
+  });
+});
+
+app.get('/students/:id', (req, res) => {
+  Student.findById(req.params.id).then(student => res.json(student));
+});
+
+app.put('/students/:id', (req, res) => {
+  Student.findByIdAndUpdate(req.params.id, req.body, { new: true }).then(student => {
+    res.json(student);
+  }).catch(err => {
+    res.status(400).json({ message: 'Error updating student', errors: err.errors });
+  });
+});
+
+app.delete('/students/:id', (req, res) => {
+  Student.findByIdAndRemove(req.params.id).then(() => res.json({ message: 'Student deleted' }));
+});
+
+const PORT = process.env.PORT || 3010;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
